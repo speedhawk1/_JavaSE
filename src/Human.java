17eaaeafb508e1953e376868a9fe41d40cb4aa15
thu <mingfei.net@gmail.com>
2016-09-10 15:08:51 +0800

    Update

---
+++ src/Human.java
@@ -0,0 +1,36 @@
+/**
+ * Created by Administrator
+ * 2016/9/10.
+ */
+// 类 = 域 + 方法
+// class = field + method
+public class Human { //
+
+//    1. fields
+
+    String name;
+    int age;
+    char gender;
+    double height;
+    double weight;
+    boolean isMarried;
+
+    //    2. methods
+
+    void studey() {
+
+    }
+
+    void work() {
+
+    }
+
+    void eat() {
+
+    }
+
+    void sleep() {
+
+    }
+
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator
+ * 2016/9/10.
+ */
+public class Vehicle { // 交通工具
+}
