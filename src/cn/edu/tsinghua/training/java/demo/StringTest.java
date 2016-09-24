290514102bc75bbd6a32f70fb5eabe948dacaaaf
thu <mingfei.net@gmail.com>
2016-09-24 16:30:19 +0800

    Update

--- src/cn/edu/tsinghua/training/java/demo/StringTest.java
+++ src/cn/edu/tsinghua/training/java/demo/StringTest.java
@@ -5,6 +5,7 @@ package cn.edu.tsinghua.training.java.demo;
  * 2016/9/24.
  */
 public class StringTest {
+
     public static void main(String[] args) {
         String s = "hello";
         System.out.println(s.charAt(1));
