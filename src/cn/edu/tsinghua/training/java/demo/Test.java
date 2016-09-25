58578aa02c5106723fd53c60bbfa634039660254
thu <mingfei.net@gmail.com>
2016-09-25 16:41:54 +0800

    Update

--- src/cn/edu/tsinghua/training/java/demo/Test.java
+++ src/cn/edu/tsinghua/training/java/demo/Test.java
@@ -1,28 +1,21 @@
 package cn.edu.tsinghua.training.java.demo;
 
 
-import demo.Human;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 //import java.lang.String; language
 
 /**
  * Created by Administrator
  * 2016/9/11.
  */
-public class Test {
+public class Test extends Object {
+
     public static void main(String[] args) {
-        Hello hello = new Hello();
-        demo.Hello hello1 = new demo.Hello();
-
-//        utility
-        Scanner scanner = new Scanner(System.in);
-        List<String> strings = new ArrayList<>();
-
-        Demo demo = new Demo();
-
+        Test test = new Test();
+        System.out.println(test);
     }
+
+//    @Override
+//    public String toString() {
+//        return "a Test class instance...";
+//    }
 }
