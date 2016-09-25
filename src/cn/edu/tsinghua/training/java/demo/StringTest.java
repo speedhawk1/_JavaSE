e9ee2863be94626d51a3a9797c41dd7dafcd3aec
thu <mingfei.net@gmail.com>
2016-09-25 14:10:21 +0800

    Update

--- src/cn/edu/tsinghua/training/java/demo/StringTest.java
+++ src/cn/edu/tsinghua/training/java/demo/StringTest.java
@@ -7,7 +7,7 @@ package cn.edu.tsinghua.training.java.demo;
 public class StringTest {
 
     private String name;
-    private static  int i;
+    private static int i;
 
     public static void main(String[] args) {
 
@@ -15,12 +15,43 @@ public class StringTest {
         StringTest.i = 1;
 
 
-        String s = "hello你好";
+        String s = "hellaaaaaaaaaaaaaaaaaaaao你好";
         System.out.println(s.charAt(s.length() - 1));
         System.out.println(s.length());
         String s1 = "hi";
         System.out.println(s + s1);
 
         System.out.println(s.contains("a")); // Ctrl + Q
+
+        System.out.println(s.indexOf("l"));
+        System.out.println(s.lastIndexOf("l"));
+
+        System.out.println(s.replace("l", "a"));
+        System.out.println(s);
+
+        String s2 = "a  b c d e";
+        String[] strings = s2.split("  ");
+        for (String string : strings) {
+            System.out.println(string);
+        }
+
+        System.out.println(s.startsWith("he"));
+        System.out.println(s.endsWith("好"));
+
+        System.out.println(s.substring(s.indexOf("o"), s.indexOf("o") + 2));
+
+        System.out.println(s.toUpperCase().toLowerCase());
+
+
+    }
+
+    public String upper(String s) {
+
+        return "";
+    }
+
+    public String lower(String s) {
+
+        return "";
     }
 }
