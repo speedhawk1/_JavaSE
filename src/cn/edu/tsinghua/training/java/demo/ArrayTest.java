fb81ac28e518894b826b905fac6df7624d558377
thu <mingfei.net@gmail.com>
2016-09-25 10:47:19 +0800

    Update

--- src/cn/edu/tsinghua/training/java/demo/ArrayTest.java
+++ src/cn/edu/tsinghua/training/java/demo/ArrayTest.java
@@ -6,7 +6,11 @@ package cn.edu.tsinghua.training.java.demo;
  */
 public class ArrayTest {
     public static void main(String[] args) {
-        int[] ints = new int[50];
+
+//        shift + alt + f10
+        System.out.println(args.length);
+
+        int[] ints = new int[10];
 
         System.out.println(ints.length); // 运行时异常 Runtime Exception
 
@@ -16,6 +20,26 @@ public class ArrayTest {
         }
 
         System.out.println(ints[ints.length - 1]);
+
+        String[] strings = {"hello", "hi", "test", "asdf..."};
+
+        for (int i = 0; i < strings.length; i++) {
+            System.out.println(i);
+            System.out.println(strings[i]);
+        }
+
+//        iter + Tab
+        for (String string : strings) {
+            System.out.println(string);
+        }
+
+        System.out.println(strings.length);
+
+        Object[] objects = new Object[10];
+        objects[0] = 1;
+        objects[1] = "hi";
+        objects[2] = new ArrayTest();
+        System.out.println(objects[0]);
 
     }
 }
---
+++ src/cn/edu/tsinghua/training/java/demo/_2DArray.java
@@ -0,0 +1,33 @@
+package cn.edu.tsinghua.training.java.demo;
+
+/**
+ * Created by Administrator
+ * 2016/9/25.
+ */
+public class _2DArray {
+    public static void main(String[] args) {
+//        int[][] ints = new int[3][];
+//        ints[0] = new int[1];
+//        ints[1] = new int[5];
+//        ints[2] = new int[2];
+
+        int[][] ints = {
+                {1},
+                {1, 2, 3, 4, 5},
+                {1, 2}
+        };
+
+        for (int i = 0; i < ints.length; i++) {
+            for (int j = 0; j < ints[i].length; j++) {
+                ints[i][j] = i + j;
+            }
+        }
+
+        System.out.println(ints.length); // 3
+        System.out.println(ints[0].length); // 1
+        System.out.println(ints[1].length); // 5
+        System.out.println(ints[2].length); // 2
+
+        String[][][] strings = new String[1][2][3];
+    }
+}
