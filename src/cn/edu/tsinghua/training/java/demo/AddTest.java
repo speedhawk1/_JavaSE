bd1c3823bbaba188fe98153dfa01fdd3ac0ab48d
thu <mingfei.net@gmail.com>
2016-10-15 10:19:33 +0800

    Update

--- src/cn/edu/tsinghua/training/java/demo/AddTest.java
+++ src/cn/edu/tsinghua/training/java/demo/AddTest.java
@@ -6,8 +6,13 @@ package cn.edu.tsinghua.training.java.demo;
  */
 public class AddTest {
 
-//    1. add int
-//    2. add double
+    //    1. add int  x=1, y=2 // 12
+    public String add(int x, int y) {
+//        return "" + x + y;
+        return String.valueOf(x) + String.valueOf(y);
+    }
+//    2. add double x=1.1, y=2.2 // 1.12.2
+//    3. add boolean x=true, y=false // truefalse
 
     public static void main(String[] args) {
         AddTest addTest = new AddTest();
