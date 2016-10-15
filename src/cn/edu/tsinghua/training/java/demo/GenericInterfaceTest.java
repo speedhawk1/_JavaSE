09318c91ee422a8933fc3ded1fca34c0dc3af7b2
thu <mingfei.net@gmail.com>
2016-10-15 11:12:43 +0800

    Update

--- src/cn/edu/tsinghua/training/java/demo/GenericInterfaceTest.java
+++ src/cn/edu/tsinghua/training/java/demo/GenericInterfaceTest.java
@@ -4,14 +4,25 @@ package cn.edu.tsinghua.training.java.demo;
  * Created by Administrator
  * 2016/10/15.
  */
-public interface GenericInterfaceTest<T> {
+public interface GenericInterfaceTest<T extends Number> {
     T m(T t);
+
+    void method();
 }
 
-class GenericInterfaceTestImpl<T> implements GenericInterfaceTest<T> {
+class GenericInterfaceTestImpl<T extends Number> implements GenericInterfaceTest<T> {
 
     @Override
     public T m(T t) {
         return null;
+    }
+
+    @Override
+    public void method() {
+
+    }
+
+    public static void main(String[] args) {
+        GenericInterfaceTestImpl<Long> test = new GenericInterfaceTestImpl<>();
     }
 }
