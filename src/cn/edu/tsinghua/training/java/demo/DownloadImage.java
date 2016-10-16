eede75f36a06340a89f1037dbc9877a0fd6780af
thu <mingfei.net@gmail.com>
2016-10-16 16:38:12 +0800

    Update

--- src/cn/edu/tsinghua/training/java/demo/DownloadImage.java
+++ src/cn/edu/tsinghua/training/java/demo/DownloadImage.java
@@ -1,9 +1,6 @@
 package cn.edu.tsinghua.training.java.demo;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.URL;
 
 /**
@@ -11,14 +8,34 @@ import java.net.URL;
  * 2016/10/16.
  */
 public class DownloadImage {
-    private static String imageURL = "http://tankr.net/s/custom/GWR5.jpg";
+
+    private static int counter;
 
     public static void main(String[] args) throws IOException {
+        String imageURL = "http://jandan.net/tag/%E5%A4%A9%E6%96%87";
         URL url = new URL(imageURL);
-        try (InputStream inputStream = url.openStream(); OutputStream outputStream = new FileOutputStream("d:/1.jpg")){
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.contains("tankr")) {
+                    System.out.println(++counter);
+                    down("http://" + line.substring(line.indexOf("tankr"), line.indexOf(" width") - 1));
+                }
+            }
+            System.out.println(counter);
+        }
+    }
+
+    public static void down(String imageURL) throws IOException {
+        URL url = new URL(imageURL);
+        String fileName = counter + ".jpg";
+        try (
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(url.openStream());
+                BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream("data/" + fileName))
+        ) {
             int i;
-            while ((i = inputStream.read()) != -1) {
-                outputStream.write(i);
+            while ((i = bufferedInputStream.read()) != -1) {
+                bufferedOutputStream.write(i);
             }
         }
     }
